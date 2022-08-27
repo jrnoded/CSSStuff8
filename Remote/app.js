b07d2cb6a17df6c856cffde8c4800da6d5386815
Jim Roberts <64096792+jrnoded@users.noreply.github.com>
2022-08-27 16:32:20 -0400

    Completed Commit

--- Remote/app.js
+++ Remote/app.js
@@ -1,6 +1,10 @@
 const path = document.getElementById("pathtext");
 const keyboard = document.getElementsByClassName("key");
-
+const inputWord = document.getElementById("inputWord");
+let rules = "Use arrow keys to move to letter of word. Cleck Sel key to select letter. ";
+rules += "Click Submit to process word finding effcient path thru all keys. ";
+rules += "Effcient search is up and down first then left and right.";
+rules += "Ok you can cheat and just type in the word. Sorry no backspace implemented!";
 const keys = {
     q: { x: 0, y: 0 },
     w: { x: 0, y: 1 },
@@ -20,6 +24,7 @@ const keys = {
     h: { x: 1, y: 5 },
     j: { x: 1, y: 6 },
     k: { x: 1, y: 7 },
+    l: { x: 1, y: 8 },
     z: { x: 2, y: 0 },
     x: { x: 2, y: 1 },
     c: { x: 2, y: 2 },
@@ -28,7 +33,8 @@ const keys = {
     n: { x: 2, y: 5 },
     m: { x: 2, y: 6 },
 };
- setSelected('q');
+
+reset();
 
 function clicked(s) {
     let currentChar = getSelected();
@@ -37,9 +43,7 @@ function clicked(s) {
     let y = key.y;
     switch (s) {
         case "clear":
-            path.innerHTML = "";
-            setSelected("q");
-            clearPicked();
+            reset();
             break;
         case "up":
             findKeyboardKey(key.x - 1, key.y);
@@ -55,15 +59,14 @@ function clicked(s) {
             break;
         case "select":
             setPicked(currentChar);
-            // let p = "";
-            // if (path.innerHTML === "") {
-            //     p = s;
-            // } else {
-            //     p = "," + s;
-            // }
-            // path.innerHTML += p;
             break;
     }
+}
+function reset() {
+    path.innerHTML = rules;
+    inputWord.value = "";
+    setSelected("q");
+    clearPicked();
 }
 function setSelected(c) {
     for (const k of keyboard) {
@@ -77,7 +80,7 @@ function setPicked(c) {
     for (const k of keyboard) {
         if (k.dataset.chr == c) {
             k.classList.add("picked");
-            path.innerHTML += c;
+            inputWord.value += c;
         }
     }
 }
@@ -95,7 +98,6 @@ function getSelected() {
     return "q";
 }
 function findKeyboardKey(x, y) {
-    console.log(x, y);
     for (const k in keys) {
         pos = keys[k];
         if (pos) {
@@ -108,31 +110,32 @@ function findKeyboardKey(x, y) {
     return "";
 }
 
-let ta = new Array();
+function submitWord() {
+    const word = inputWord.value;
 
-// let word = "car";
-// currentkey = keys["q"];
+    let currentkey = keys["q"];
+    let ta = new Array();
+    for (let c of word) {
+        let key = keys[c];
 
-// for (let c of word) {
-//     let key = keys[c];
-
-//     let vertical = key.x - currentkey.x;
-//     let horizontal = key.y - currentkey.y;
-//     let vert = "down";
-//     let horz = "right";
-//     if (vertical < 0) vert = "up";
-//     if (horizontal < 0) horz = "left";
-//     vertical = Math.abs(vertical);
-//     while (vertical > 0) {
-//         ta.push(vert);
-//         vertical--;
-//     }
-//     horizontal = Math.abs(horizontal);
-//     while (horizontal > 0) {
-//         ta.push(horz);
-//         horizontal--;
-//     }
-//     currentkey = key;
-//     ta.push("Select");
-// }
-// path.innerHTML = word + " " + String(ta.join(", ")).toLowerCase();
+        let vertical = key.x - currentkey.x;
+        let horizontal = key.y - currentkey.y;
+        let vert = "down";
+        let horz = "right";
+        if (vertical < 0) vert = "up";
+        if (horizontal < 0) horz = "left";
+        vertical = Math.abs(vertical);
+        while (vertical > 0) {
+            ta.push(vert);
+            vertical--;
+        }
+        horizontal = Math.abs(horizontal);
+        while (horizontal > 0) {
+            ta.push(horz);
+            horizontal--;
+        }
+        currentkey = key;
+        ta.push("Select");
+    }
+    path.innerHTML = `${word}: ${String(ta.join(", ")).toLowerCase()}`;
+}
